87b98b28e4109b1f2b1e1a76e36c184563f07eb0
Yannis Karakozis <I-C-Karakozis@users.noreply.github.com>
2019-05-22 11:24:17 -0400

    Chapter 0

---
+++ C0/hello.cpp
@@ -0,0 +1,8 @@
+// my first C++ program
+#include <iostream>
+
+int main() {
+	std::cout << "Hello, world!" << std::endl;
+	std::cout << "This (\") is a quote,\tand this (\\) is a backslash." << std::endl;
+	return 0;
+}
